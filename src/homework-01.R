b654fd88158a355720af69f3536e4c9b69661d5f
dankaella <ddaniella.nagy@gmail.com>
2017-02-22 23:27:40 +0100

    elso resz

--- src/homework-01.R
+++ src/homework-01.R
@@ -5,3 +5,14 @@
 # Nagy Daniella               #
 # 2017.02.22.                 #
 ###############################
+
+#--------- II. feladat --------
+# 1. Package installálása
+if (!("fivethirtyeight" %in% installed.packages())) {
+  install.packages("fivethirtyeight")
+}
+library("fivethirtyeight")
+# 2. Dataset megkeresése és beolvasása
+data(package = "fivethirtyeight")
+data("tarantino")
+head("tarantino")
